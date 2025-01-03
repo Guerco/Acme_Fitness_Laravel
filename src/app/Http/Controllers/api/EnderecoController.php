9b05767bf2afa59be194e17e861263748ec1beb7
Guerco <gabrielszgrco@gmail.com>
2025-01-03 00:39:39 -0300

    Update EnderecoController
    Correção do controller de endereço

--- src/app/Http/Controllers/api/EnderecoController.php
+++ src/app/Http/Controllers/api/EnderecoController.php
@@ -3,36 +3,53 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\EnderecoRequest;
+use App\Http\Services\EnderecoService;
 use App\Models\Endereco;
 use App\Http\Resources\EnderecoResource;
 
 class EnderecoController extends ControllerBase
 {
-    public function getAll () {
-        $enderecos = Endereco::all();
-
-        return EnderecoResource::collection($enderecos);
+    public function __construct ( EnderecoService $service ) {
+        parent::__construct( $service );
+    }
+    
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return parent::_index();
     }
 
-    public function findById  ( $id ) {
-        $endereco = Endereco::find( $id );
-
-        return new EnderecoResource( $endereco );
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store( EnderecoRequest $request )
+    {
+        return parent::_store( $request );
     }
 
-    public function create ( EnderecoRequest $request ) {
-        Endereco::create( $request->validated() );
+    /**
+     * Display the specified resource.
+     */
+    public function show( $id )
+    {
+        return parent::_show( $id );
     }
 
-    public function edit ( EnderecoRequest $request, $id ) {
-        $endereco = Endereco::findOrFail( $id  );
-
-        $endereco->update( $request->validated() );
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update( EnderecoRequest $request, $id )
+    {
+        return parent::_update( $request, $id );
     }
 
-    public function remove ( $id ) {
-        $endereco = Endereco::findOrFail( $id );
-
-        $endereco->delete();
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy( $id )
+    {
+        return parent::_destroy( $id );
     }
 }
