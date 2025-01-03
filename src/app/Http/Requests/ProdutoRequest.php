ad53a1565781c4891b2be2e9ec3c6055bd283331
Guerco <gabrielszgrco@gmail.com>
2025-01-03 09:22:23 -0300

    Create ProdutoRequest
    Criação do FormRequest de cliente, responsável pela validação dos dados enviados

---
+++ src/app/Http/Requests/ProdutoRequest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProdutoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $rules = [
+            
+            'nome' =>   [
+                            'required', 
+                            'max:50',
+                        ] ,
+            'imagem_path' =>   [
+                            'nullable', 
+                            'max:200',
+                        ] ,
+            'descricao' => [
+                                'nullable' ,
+                            ] ,
+            'data_cadastro' =>  [
+                                    'nullable',
+                                    'date',
+                                    'date_format:Y-m-d',
+                                ] ,   
+            'categoria.id' =>   [
+                                    'required' , 
+                                    'integer'
+                                ] ,
+        ];
+
+
+        if ($this->method() === 'PUT') {
+            $rules = [
+            
+                'nome' =>   [
+                                'nullable', 
+                                'max:50',
+                            ] ,
+                'imagem_path' =>   [
+                                'nullable', 
+                                'max:200',
+                            ] ,
+                'descricao' => [
+                                    'nullable' ,
+                                ] ,
+                'data_cadastro' =>  [
+                                        'nullable',
+                                        'date',
+                                        'date_format:Y-m-d',
+                                    ] ,   
+                'categoria.id' =>   [
+                                        'nullable' , 
+                                        'integer'
+                                    ] ,
+            ];
+        }
+
+        return $rules;
+    }
+}
