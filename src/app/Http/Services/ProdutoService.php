ca3a03f30c76b21927339555537a8081d1c60d11
Guerco <gabrielszgrco@gmail.com>
2025-01-03 09:22:57 -0300

    Create ProdutoService
    Criação do service de produto, responsável pelas interações com model e banco de dados, e concentração das regras de negócio

---
+++ src/app/Http/Services/ProdutoService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Http\Requests\ProdutoRequest;
+use App\Models\Produto;
+use App\Http\Resources\ProdutoResource;
+
+
+class ProdutoService {
+    public function getAll () {
+        $produtos = Produto::with('categoria')->get();
+
+        return ProdutoResource::collection($produtos);
+    }
+
+    public function findById  ( $id ) {
+        $produto = Produto::with('categoria')->findOrFail( $id );
+
+        return new ProdutoResource( $produto );
+    }
+
+    public function create ( ProdutoRequest $request ) {
+        $dados = $request->validated();
+        $dados['categoria_id'] = $request['categoria']['id'];
+        
+        $dados['data_cadastro'] = $dados['data_cadastro'] ?? today();
+
+        Produto::create( $dados);
+    }
+    
+    public function edit ( ProdutoRequest $request, $id ) {
+        $produto = Produto::findOrFail( $id  );
+
+        $dados = $request->validated();
+        if (isset($request['categoria']['id'])) {
+            $dados['categoria'] = $request['categoria']['id'];
+        }
+
+        $produto->update( $dados );
+    }
+
+    public function remove ( $id ) {
+        $produto = Produto::findOrFail( $id );
+
+        $produto->delete();
+    }
+
+}
