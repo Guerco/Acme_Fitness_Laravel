2964cd0c749444679478a560d8b0350487c113dd
Guerco <gabrielszgrco@gmail.com>
2025-01-02 23:14:46 -0300

    Add apiResource categorias to \routes\api
    Adição das rotas do recurso de categorias ao arquivo de rotas da api

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,3 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CategoriaController;
+
+
+Route::apiResource( 'categorias',  CategoriaController::class)->only (
+    [
+        'index', 
+        'show', 
+        'store', 
+        'update', 
+        'destroy',
+    ]
+);
+
