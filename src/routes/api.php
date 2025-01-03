602b5671aa509b8f9cc0bb5c676078a331509138
Guerco <gabrielszgrco@gmail.com>
2025-01-03 09:27:25 -0300

    Add apiResource enderecos to \routes\api
    Adição das rotas do recurso de enderecos ao arquivo de rotas da api

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoriaController;
 use App\Http\Controllers\Api\ClienteController;
+use App\Http\Controllers\Api\EnderecoController;
+use App\Http\Controllers\Api\ProdutoController;
 
 
 Route::apiResource( 'categorias',  CategoriaController::class)->only (
@@ -17,6 +19,16 @@ Route::apiResource( 'categorias',  CategoriaController::class)->only (
 );
 
 Route::apiResource( 'clientes',  ClienteController::class)->only (
+    [
+        'index', 
+        'show', 
+        'store', 
+        'update', 
+        'destroy',
+    ]
+);
+
+Route::apiResource( 'enderecos',  EnderecoController::class)->only (
     [
         'index', 
         'show', 
