3fbd4b46616d5b1c8f633375385f70837dc0dd73
Guerco <gabrielszgrco@gmail.com>
2025-01-03 00:13:51 -0300

    Create EnderecoRequest
    Criação do FormRequest de endereco, responsável pela validação dos dados enviados

---
+++ src/app/Http/Requests/EnderecoRequest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EnderecoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $rules = [
+            'logradouro' => [
+                                'required',
+                                'max:50'
+                            ] ,
+            'cidade' => [
+                            'required',
+                            'max:50'
+                        ] ,
+            'bairro' => [
+                            'required',
+                            'max:50'
+                        ] ,
+            'numero' =>    [
+                            'required',
+                            'max:10'
+                        ] ,
+            'cep' =>    [
+                            'required',
+                            'regex:/^\d{5}\-\d{3}$/'
+                        ] ,
+            'complemento' => [
+                            'nullable',
+                            'max:50'
+                        ] ,
+        ];
+
+        if ($this->method() === 'PUT') {
+            $rules = [
+                'logradouro' => [
+                                    'nullable',
+                                    'max:50'
+                                ] ,
+                'cidade' => [
+                                'nullable',
+                                'max:50'
+                            ] ,
+                'bairro' => [
+                                'nullable',
+                                'max:50'
+                            ] ,
+                'numero' =>    [
+                                'nullable',
+                                'max:10'
+                            ] ,
+                'cep' =>    [
+                                'nullable',
+                                'regex:/^\d{5}\-\d{3}$/'
+                            ] ,
+                'complemento' => [
+                                'nullable',
+                                'max:50'
+                            ] ,  
+            ];
+        }
+
+        return $rules;
+    }
+}
