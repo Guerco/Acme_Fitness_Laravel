83c6951fd95a35725660afd2bd409558bb21ccba
Guerco <gabrielszgrco@gmail.com>
2025-01-02 23:21:16 -0300

    Create ClienteController
    Criação do controlador de cliente, responsável por gerenciar as requisições e encaminhá-las ao service

---
+++ src/app/Http/Controllers/api/ClienteController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ClienteRequest;
+use App\Http\Services\ClienteService;
+
+class ClienteController extends ControllerBase
+{
+
+    public function __construct ( ClienteService $service ) {
+        parent::__construct( $service );
+    }
+    
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return parent::_index();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store( ClienteRequest $request )
+    {
+        return parent::_store( $request );
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show( $id )
+    {
+        return parent::_show( $id );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update( ClienteRequest $request, $id )
+    {
+        return parent::_update( $request, $id );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy( $id )
+    {
+        return parent::_destroy( $id );
+    }
+
+}
