a67d3ccec9bb42b2338f9e29c574f5acb9969206
Guerco <gabrielszgrco@gmail.com>
2025-01-03 15:30:57 -0300

    Add custom exception handling
    Declração de comportamentos personalizados para exceptions específicas

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Middleware\ForceJsonResponse;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\ErrorHandler\Error\FatalError;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Validation\ValidationException;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,5 +21,53 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append(ForceJsonResponse::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+
+        $exceptions->render( function ( NotFoundHttpException $e  ) {
+            if ( str_starts_with( $e->getMessage() , 'The route') ) {
+                $response['message'] = 'Recurso Indisponível.';
+                
+                if ( env('APP_DEBUG') == 'true' ) {
+                    $response['errors'] = $e->getMessage();
+                }
+
+                return response()->json(
+                    $response, 
+                    404,
+                    [],
+                    JSON_PRETTY_PRINT
+                );
+            }
+            
+            if ( str_starts_with( $e->getMessage() , 'No query results for model') ) {
+                $response['message'] = 'Entidade não encontrada.';
+                
+                if ( env('APP_DEBUG') == 'true' ) {
+                    $response['errors'] = $e->getMessage();
+                }
+
+                return response()->json(
+                    $response, 
+                    404,
+                    [],
+                    JSON_PRETTY_PRINT
+                );
+            }
+        }); 
+
+        $exceptions->render( function (ValidationException $e )  { 
+            $response['message'] = 'Erro de Validação.';
+            
+            if ( env('APP_DEBUG') == 'true' ) {
+                $response['errors'] = $e->errors();
+            }
+
+            return response()->json(
+                $response, 
+                404,
+                [],
+                JSON_PRETTY_PRINT
+            );
+        } );
+
     })->create();
+    
